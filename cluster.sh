54e2848e70af5d7a968d9fc8f30ce84749114ef8
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2017-03-07 13:24:20 -0800

    install mysql

--- cluster.sh
+++ cluster.sh
@@ -1,10 +1,20 @@
-yum update
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum -y update
 yum install -y pacemaker corosync
+
+# set up corosync
 cp /root/shared/example-authkey /etc/corosync/authkey
 cp /root/shared/corosync.conf /etc/corosync/corosync.conf
 ip=`hostname -I | awk '/192.168.101/{print $NF}'`
 sed -ie "s/192.168.101.xxx/$ip/g" /etc/corosync/corosync.conf
 systemctl start corosync
+
+# set up pacemaker
 mkdir /etc/corosync/service.d/
 cp /root/shared/pacemaker /etc/corosync/service.d/pacemaker
 systemctl start pacemaker
+
+# set up mysql
+yum install -y mysql-server
+systemctl start mysqld
